f8e0be85f9f96f7510aaa83348c61cd4daab6292
kseniakonaeva <konaevaksusa7@gmail.com>
2024-10-19 01:15:39 +0300

    step2

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 # количество клеток
 board_size = 3
+step = 1
 # игровое поле
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -12,6 +13,44 @@ def draw_board():
         print(('_' * 3 + '|') * 3)
         pass
 
+
+
+def start_game():
+    # текущий игрок
+    current_player = 'X'
+    # номер шага
+    step = 1
+    draw_board()
+
+def game_step(index, char):
+    """ выполняем ход """
+    if (index > 9 or index < 1 or board[index - 1] in ('X', 'O')):
+        return False
+    board[index - 1] = char
+    return True
+
+
+def check_win():
+    """ Проверяем победу одного из игроков """
+    while (step < 10) and (check_win() == False):
+        index = input('Ходит игрок ' + current_player + '. Введите номер поля (0 - выход):')
+        if (index == '0'):
+            break
+        # Проверка ввода
+        try:
+            index = int(index)
+        except ValueError:
+            print('Неверный ввод! Повторите!')
+            continue
+
+        # если получилось сделать шаг
+        if (game_step(index, current_player)):
+            print('Удачный ход')
+
+            if (current_player == 'X'):
+                current_player = 'O'
+            else:
+                current_player = 'X'
 def start_game():
     draw_board()
 
