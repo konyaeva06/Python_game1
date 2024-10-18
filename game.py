352ba54e0e3c3e993a21e2182e9679c1b5159f7c
kseniakonaeva <konaevaksusa7@gmail.com>
2024-10-19 01:37:48 +0300

    step4

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 # количество клеток
 board_size = 3
-step = 1
 # игровое поле
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 
 def draw_board():
     """ Выводим игровое поле """
@@ -11,23 +11,15 @@ def draw_board():
         print((' ' * 3 + '|') * 3)
         print('', board[i * 3], '|', board[1 + i * 3], '|', board[2 + i * 3], '|')
         print(('_' * 3 + '|') * 3)
-        pass
 
-
-
-def start_game():
-    # текущий игрок
-    current_player = 'X'
-    # номер шага
-    step = 1
-    draw_board()
 
 def game_step(index, char):
     """ выполняем ход """
-    if (index > 9 or index < 1 or board[index - 1] in ('X', 'O')):
+    if index > 9 or index < 1 or board[index - 1] in ('X', 'O'):
         return False
     board[index - 1] = char
     return True
+
 
 def check_win():
     """ Проверяем победу одного из игроков """
@@ -38,16 +30,23 @@ def check_win():
     )
 
     for pos in win_combination:
-        if (board[pos[0]] == board[pos[1]] == board[pos[2]]):
+        if board[pos[0]] == board[pos[1]] == board[pos[2]]:
             return board[pos[0]]
     return False
 
-def check_win():
-    """ Проверяем победу одного из игроков """
-    while (step < 10) and (check_win() == False):
-        index = input('Ходит игрок ' + current_player + '. Введите номер поля (0 - выход):')
-        if (index == '0'):
+
+def start_game():
+    # текущий игрок
+    current_player = 'X'
+    # номер шага
+    step = 1
+    draw_board()
+
+    while step < 10 and check_win() == False:
+        index = input(f'Ходит игрок {current_player}. Введите номер поля (0 - выход): ')
+        if index == '0':
             break
+
         # Проверка ввода
         try:
             index = int(index)
@@ -56,16 +55,27 @@ def check_win():
             continue
 
         # если получилось сделать шаг
-        if (game_step(index, current_player)):
+        if game_step(index, current_player):
             print('Удачный ход')
 
-            if (current_player == 'X'):
+            if current_player == 'X':
                 current_player = 'O'
             else:
                 current_player = 'X'
-def start_game():
-    draw_board()
+
+            draw_board()
+            # увеличим номер хода
+            step += 1
+        else:
+            print('Неверный номер! Повторите!')
+
+    if step == 10:
+        print('Игра окончена. Ничья!')
+    else:
+        print(f'Выиграл {check_win()}')
+
 
 print('Добро пожаловать в крестики-нолики!')
 start_game()
+
 
